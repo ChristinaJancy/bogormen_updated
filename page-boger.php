4a67ee231eafad85169cfb9212106f9ab4e29d42
Christina <chri91eq@easv365.dk>
2020-12-12 19:42:56 +0100

    updated product pages - html and css

--- page-boger.php
+++ page-boger.php
@@ -10,21 +10,101 @@ get_header(); // This fxn gets the header.php file and renders it
 </div>
 	<h2 class="sub-banner">Bøger</h2>
 
-
+<section class="section-products">
 <div class="row" id="products">
-	<div class="col-6 col-s-12 product">
-		<img src="https://4.imimg.com/data4/VO/DP/MY-3816229/harry-potter-and-the-cursed-child-parts-i-500x500.jpg">
+	<div class="col-6 col-s-12 product-container">
+		<div class="row product">
+			<div class="col-8 col-s-12 product-col1">
+			<img src="https://www.zoom.co.uk/assets/images/site/product/8320448_1.jpg?width=1200">
+			<p><b>Peppa pig</b></p>
+			</div>
+			<div class="col-4 col-s-12 product-col2">
+			<p>På lager</p>
+			<p>100 dkk.-</p>
+			</div>
+		</div>	
 	</div>
-	<div class="col-6 col-s-12 product">
-		<img src="https://4.imimg.com/data4/VO/DP/MY-3816229/harry-potter-and-the-cursed-child-parts-i-500x500.jpg">
+	
+<!-- 	 -->
+	
+	<div class="col-6 col-s-12 product-container">
+		<div class="row product">
+			<div class="col-8 col-s-12 product-col1">
+			<img src="https://www.zoom.co.uk/assets/images/site/product/8320448_1.jpg?width=1200">
+			<p><b>Peppa pig</b></p>
+			</div>
+			<div class="col-4 col-s-12 product-col2">
+			<p>På lager</p>
+			<p>100 dkk.-</p>
+			</div>
+		</div>	
 	</div>
-	<div class="col-6 col-s-12 product">
-		<img src="https://4.imimg.com/data4/VO/DP/MY-3816229/harry-potter-and-the-cursed-child-parts-i-500x500.jpg">
+	
+	
+<!-- 	 -->
+	
+	<div class="col-6 col-s-12 product-container">
+		<div class="row product">
+			<div class="col-8 col-s-12 product-col1">
+			<img src="https://www.zoom.co.uk/assets/images/site/product/8320448_1.jpg?width=1200">
+			<p><b>Peppa pig</b></p>
+			</div>
+			<div class="col-4 col-s-12 product-col2">
+			<p>På lager</p>
+			<p>100 dkk.-</p>
+			</div>
+		</div>	
 	</div>
-	<div class="col-6 col-s-12 product">
-		<img src="https://4.imimg.com/data4/VO/DP/MY-3816229/harry-potter-and-the-cursed-child-parts-i-500x500.jpg" >
+	
+	
+<!-- 	 -->
+	
+	<div class="col-6 col-s-12 product-container">
+		<div class="row product">
+			<div class="col-8 col-s-12 product-col1">
+			<img src="https://www.zoom.co.uk/assets/images/site/product/8320448_1.jpg?width=1200">
+			<p><b>Peppa pig</b></p>
+			</div>
+			<div class="col-4 col-s-12 product-col2">
+			<p>På lager</p>
+			<p>100 dkk.-</p>
+			</div>
+		</div>	
 	</div>
-</div>
+	
+	
+<!-- 	 -->
+	
+	<div class="col-6 col-s-12 product-container">
+		<div class="row product">
+			<div class="col-8 col-s-12 product-col1">
+			<img src="https://www.zoom.co.uk/assets/images/site/product/8320448_1.jpg?width=1200">
+			<p><b>Peppa pig</b></p>
+			</div>
+			<div class="col-4 col-s-12 product-col2">
+			<p>På lager</p>
+			<p>100 dkk.-</p>
+			</div>
+		</div>	
+	</div>
+	
+	
+<!-- 	 -->
+	
+	<div class="col-6 col-s-12 product-container">
+		<div class="row product">
+			<div class="col-8 col-s-12 product-col1">
+			<img src="https://www.zoom.co.uk/assets/images/site/product/8320448_1.jpg?width=1200">
+			<p><b>Peppa pig</b></p>
+			</div>
+			<div class="col-4 col-s-12 product-col2">
+			<p>På lager</p>
+			<p>100 dkk.-</p>
+			</div>
+		</div>	
+	</div>
+	</div>
+</section>
 
 <?php 'get_footer'(); // This fxn gets the footer.php file and renders it 
 ?>
